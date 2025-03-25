e361fb0aae7ddddfe8ab11a61c3ac1718de73954
rishiparashar02 <rishivparashar@gmail.com>
2025-03-25 17:26:37 +0530

    logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,72 +6,78 @@ import toast, { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
 import fetchUserDetails from './utils/fetchUserDetails';
 import { setUserDetails } from './store/userSlice';
-import { setAllCategory,setAllSubCategory,setLoadingCategory } from './store/productSlice';
+import { setAllCategory, setAllSubCategory, setLoadingCategory } from './store/productSlice';
 import { useDispatch } from 'react-redux';
 import Axios from './utils/Axios';
 import SummaryApi from './common/SummaryApi';
 import GlobalProvider from './provider/GlobalProvider';
+import React from 'react';
+import Logo from './assets/Image/Logo.png'
 
 function App() {
   const dispatch = useDispatch()
   const location = useLocation()
-  
 
-  const fetchUser = async()=>{
-      const userData = await fetchUserDetails()
-      dispatch(setUserDetails(userData.data))
+
+  const fetchUser = async () => {
+    const userData = await fetchUserDetails()
+    dispatch(setUserDetails(userData.data))
   }
 
-  const fetchCategory = async()=>{
+  const fetchCategory = async () => {
     try {
-        dispatch(setLoadingCategory(true))
-        const response = await Axios({
-            ...SummaryApi.getCategory
-        })
-        const { data : responseData } = response
+      dispatch(setLoadingCategory(true))
+      const response = await Axios({
+        ...SummaryApi.getCategory
+      })
+      const { data: responseData } = response
 
-        if(responseData.success){
-           dispatch(setAllCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
-        }
+      if (responseData.success) {
+        dispatch(setAllCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name))))
+      }
     } catch (error) {
-        
-    }finally{
+
+    } finally {
       dispatch(setLoadingCategory(false))
     }
   }
 
-  const fetchSubCategory = async()=>{
+  const fetchSubCategory = async () => {
     try {
-        const response = await Axios({
-            ...SummaryApi.getSubCategory
-        })
-        const { data : responseData } = response
+      const response = await Axios({
+        ...SummaryApi.getSubCategory
+      })
+      const { data: responseData } = response
 
-        if(responseData.success){
-           dispatch(setAllSubCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
-        }
+      if (responseData.success) {
+        dispatch(setAllSubCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name))))
+      }
     } catch (error) {
-        
-    }finally{
+
+    } finally {
     }
   }
 
-  
 
-  useEffect(()=>{
+
+  useEffect(() => {
     fetchUser()
     fetchCategory()
     fetchSubCategory()
-  },[])
+  }, [])
 
   return (
-    <GlobalProvider> 
-      <Header/>
-      <main className='min-h-[78vh]'>
-          <Outlet/>
-      </main>
-      <Footer/>
-      <Toaster/>
+    <GlobalProvider>
+      <Header />
+      <div className="App">
+        <main className='min-h-[78vh]'>
+          <Outlet />
+        </main>
+        <img src={Logo} alt="Logo"/>
+        {/* Rest of your app */}
+      </div>
+      <Footer />
+      <Toaster />
       {
         location.pathname !== '/checkout'
       }
