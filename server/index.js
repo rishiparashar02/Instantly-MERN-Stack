11482a4db71ab067c56fca8f203c05a40cc5ec18
rishiparashar02 <rishivparashar@gmail.com>
2025-06-11 14:47:46 +0530

    developing and changing

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import helmet from 'helmet'
 import connectDB from './config/connectDB.js'
 import userRouter from './route/user.route.js'
 
-
 const app = express()
-
+app.use(cors({
+    credentials : true,
+    origin : process.env.FRONTEND_URL
+}))
 app.use(express.json())
+app.use(cookieParser())
+app.use(morgan("combined"));
+app.use(helmet({
+    crossOriginResourcePolicy : false
+}))
 
 const PORT = process.env.PORT || 3000
 
@@ -23,4 +36,3 @@ connectDB().then(()=>{
         console.log("Server is running",PORT)
     })
 })
-
