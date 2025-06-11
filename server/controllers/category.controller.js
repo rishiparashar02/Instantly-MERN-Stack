272705f63240e2c47b75b727ac95002399f0ac3a
rishiparashar02 <rishivparashar@gmail.com>
2025-06-12 00:08:39 +0530

    developing and changing

---
+++ server/controllers/category.controller.js
@@ -0,0 +1,46 @@
+import CategoryModel from "../models/category.model.js";
+import SubCategoryModel from "../models/subCategory.model.js";
+import ProductModel from "../models/product.model.js";
+
+export const AddCategoryController = async(request,response)=>{
+    try {
+        const { name , image } = request.body 
+
+        if(!name || !image){
+            return response.status(400).json({
+                message : "Enter required fields",
+                error : true,
+                success : false
+            })
+        }
+
+        const addCategory = new CategoryModel({
+            name,
+            image
+        })
+
+        const saveCategory = await addCategory.save()
+
+        if(!saveCategory){
+            return response.status(500).json({
+                message : "Not Created",
+                error : true,
+                success : false
+            })
+        }
+
+        return response.json({
+            message : "Add Category",
+            data : saveCategory,
+            success : true,
+            error : false
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
