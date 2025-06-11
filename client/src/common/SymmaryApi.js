300c9e0d2c7655441ab1709a98810391a8655db2
rishiparashar02 <rishivparashar@gmail.com>
2025-06-11 17:09:53 +0530

    developing and changing

---
+++ client/src/common/SymmaryApi.js
@@ -0,0 +1,38 @@
+export const baseURL = import.meta.env.VITE_API_URL
+
+const SummaryApi = {
+    register : {
+        url : '/api/user/register',
+        method : 'post'
+    },
+    login : {
+        url : '/api/user/login',
+        method : 'post'
+    },
+    forgot_password : {
+        url : "/api/user/forgot-password",
+        method : 'put'
+    },
+    forgot_password_otp_verification : {
+        url : 'api/user/verify-forgot-password-otp',
+        method : 'put'
+    },
+    resetPassword : {
+        url : "/api/user/reset-password",
+        method : 'put'
+    },
+    refreshToken : {
+        url : 'api/user/refresh-token',
+        method : 'post'
+    },
+    userDetails : {
+        url : '/api/user/user-details',
+        method : "get"
+    },
+    logout : {
+        url : "/api/user/logout",
+        method : 'get'
+    },
+}
+
+export default SummaryApi
