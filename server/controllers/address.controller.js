89fa440f1cadc428473956165576b4da783bd166
rishiparashar02 <rishivparashar@gmail.com>
2025-06-13 20:45:12 +0530

    changing files

--- server/controllers/address.controller.js
+++ server/controllers/address.controller.js
@@ -3,11 +3,14 @@ import AddressModel from "../models/address.model.js";
 export const addAddressController = async(request,response)=>{
     try {
         const userId = request.userId // middleware
-        const { address_line , city,mobile } = request.body
+        const { address_line , city, state, pincode, country,mobile } = request.body
 
         const createAddress = new AddressModel({
             address_line,
             city,
+            state,
+            country,
+            pincode,
             mobile,
             userId : userId 
         })
