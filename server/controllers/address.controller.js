0c386e56fe889073e7a16233b8bb1574aa6b3b74
rishiparashar02 <rishivparashar@gmail.com>
2025-06-13 20:44:32 +0530

    changing files

---
+++ server/controllers/address.controller.js
@@ -0,0 +1,29 @@
+import AddressModel from "../models/address.model.js";
+
+export const addAddressController = async(request,response)=>{
+    try {
+        const userId = request.userId // middleware
+        const { address_line , city,mobile } = request.body
+
+        const createAddress = new AddressModel({
+            address_line,
+            city,
+            mobile,
+            userId : userId 
+        })
+
+        return response.json({
+            message : "Address Created Successfully",
+            error : false,
+            success : true,
+            data : saveAddress
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
