f0e43d4534c1c2f5a0270b9467e045791990c86a
rishiparashar02 <rishivparashar@gmail.com>
2025-06-14 15:10:01 +0530

    changing files

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,36 @@
+export const createProductController = async(request,response)=>{
+    try {
+        const { 
+            name ,
+            image ,
+            category,
+            subCategory,
+            unit,
+            price,
+            description,
+        } = request.body 
+
+        if(!name || !image[0] || !category[0] || !subCategory[0] || !unit || !price || !description ){
+            return response.status(400).json({
+                message : "Enter required fields",
+                error : true,
+                success : false
+            })
+        }
+
+        return response.json({
+            message : "Product Created Successfully",
+            data : saveProduct,
+            error : false,
+            success : true
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
