fcaf9d0493c295e5adfecccb0c52c53f55f25d1f
rishiparashar02 <rishivparashar@gmail.com>
2025-06-14 15:29:22 +0530

    changing files

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -177,3 +177,26 @@ export const getProductByCategoryAndSubCategory  = async(request,response)=>{
         })
     }
 }
+
+export const getProductDetails = async(request,response)=>{
+    try {
+        const { productId } = request.body 
+
+        const product = await ProductModel.findOne({ _id : productId })
+
+
+        return response.json({
+            message : "product details",
+            data : product,
+            error : false,
+            success : true
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
