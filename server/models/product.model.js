a0fbbbc1cf3867b43a416b09c69783303ed558e0
rishiparashar02 <rishivparashar@gmail.com>
2025-06-14 15:32:20 +0530

    changing files

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -19,7 +19,35 @@ const productSchema = new mongoose.Schema({
             type : mongoose.Schema.ObjectId,
             ref : 'subCategory'
         }
-    ]
+    ],
+    unit : {
+        type : String,
+        default : ""
+    },
+    stock : {
+        type : Number,
+        default : null
+    },
+    price : {
+        type : Number,
+        defualt : null
+    },
+    discount : {
+        type : Number,
+        default : null
+    },
+    description : {
+        type : String,
+        default : ""
+    },
+    more_details : {
+        type : Object,
+        default : {}
+    },
+    publish : {
+        type : Boolean,
+        default : true
+    }
 },{
     timestamps : true
 })
