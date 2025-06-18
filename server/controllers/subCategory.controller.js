ebae78d159af1409f0532c0fb27bef4a63f60c4a
rishiparashar02 <rishivparashar@gmail.com>
2025-06-18 15:03:50 +0530

    changing files

--- server/controllers/subCategory.controller.js
+++ server/controllers/subCategory.controller.js
@@ -55,3 +55,39 @@ export const getSubCategoryController = async(request,response)=>{
     }
 }
 
+export const updateSubCategoryController = async(request,response)=>{
+    try {
+        const { _id, name, image,category } = request.body 
+
+        const checkSub = await SubCategoryModel.findById(_id)
+
+        if(!checkSub){
+            return response.status(400).json({
+                message : "Check your _id",
+                error : true,
+                success : false
+            })
+        }
+
+        const updateSubCategory = await SubCategoryModel.findByIdAndUpdate(_id,{
+            name,
+            image,
+            category
+        })
+
+        return response.json({
+            message : 'Updated Successfully',
+            data : updateSubCategory,
+            error : false,
+            success : true
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false 
+        })
+    }
+}
+
