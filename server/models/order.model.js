132cf57339612585aef1719b17414bb2a003186b
rishiparashar02 <rishivparashar@gmail.com>
2025-06-21 21:28:12 +0530

    changing files

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -10,6 +10,38 @@ const orderSchema = new mongoose.Schema({
         required : [true, "Provide orderId"],
         unique : true
     },
+    productId : {
+        type : mongoose.Schema.ObjectId,
+        ref : "product"
+    },
+    product_details : {
+        name : String,
+        image : Array,
+    },
+    paymentId : {
+        type : String,
+        default : ""
+    },
+    payment_status : {
+        type : String,
+        default : ""
+    },
+    delivery_address : {
+        type : mongoose.Schema.ObjectId,
+        ref : 'address'
+    },
+    subTotalAmt : {
+        type : Number,
+        default : 0
+    },
+    totalAmt : {
+        type : Number,
+        default : 0
+    },
+    invoice_receipt : {
+        type : String,
+        default : ""
+    }
 },{
     timestamps : true
 })
