bada0e824aa832e09fba05900b88edaff90eb745
rishiparashar02 <rishivparashar@gmail.com>
2025-06-23 14:38:42 +0530

    changing files

--- server/controllers/subCategory.controller.js
+++ server/controllers/subCategory.controller.js
@@ -91,3 +91,23 @@ export const updateSubCategoryController = async(request,response)=>{
     }
 }
 
+export const deleteSubCategoryController = async(request,response)=>{
+    try {
+        const { _id } = request.body 
+        console.log("Id",_id)
+        const deleteSub = await SubCategoryModel.findByIdAndDelete(_id)
+
+        return response.json({
+            message : "Deleted successfully",
+            data : deleteSub,
+            error : false,
+            success : true
+        })
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
