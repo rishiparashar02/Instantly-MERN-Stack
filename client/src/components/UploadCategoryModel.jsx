3a4ed7d61bcf38e2c12df269b13b3353c0111b30
rishiparashar02 <rishivparashar@gmail.com>
2025-06-23 14:41:58 +0530

    changing files

--- client/src/components/UploadCategoryModel.jsx
+++ client/src/components/UploadCategoryModel.jsx
@@ -47,6 +47,23 @@ const UploadCategoryModel = ({close, fetchData}) => {
         }
     }
 
+    const handleUploadCategoryImage = async(e)=>{
+        const file = e.target.files[0]
+
+        if(!file){
+            return
+        }
+
+        const response = await uploadImage(file)
+        const { data : ImageResponse } = response
+
+        setData((preve)=>{
+            return{
+                ...preve,
+                image : ImageResponse.data.url
+            }
+        })
+    }
   return (
     <section className='fixed top-0 bottom-0 left-0 right-0 p-4 bg-neutral-800 bg-opacity-60 flex items-center justify-center'>
         <div className='bg-white max-w-4xl w-full p-4 rounded'>
@@ -69,7 +86,34 @@ const UploadCategoryModel = ({close, fetchData}) => {
                         className='bg-blue-50 p-2 border border-blue-100 focus-within:border-primary-200 outline-none rounded'
                     />
                 </div>
+                <div className='grid gap-1'>
+                    <p>Image</p>
+                    <div className='flex gap-4 flex-col lg:flex-row items-center'>
+                        <div className='border bg-blue-50 h-36 w-full lg:w-36 flex items-center justify-center rounded'>
+                            {
+                                data.image ? (
+                                    <img
+                                        alt='category'
+                                        src={data.image}
+                                        className='w-full h-full object-scale-down'
+                                    />
+                                ) : (
+                                    <p className='text-sm text-neutral-500'>No Image</p>
+                                )
+                            }
+                            
+                        </div>
+                        <label htmlFor='uploadCategoryImage'>
+                            <div  className={`
+                            ${!data.name ? "bg-gray-300" : "border-primary-200 hover:bg-primary-100" }  
+                                px-4 py-2 rounded cursor-pointer border font-medium
+                            `}>Upload Image</div>
 
+                            <input disabled={!data.name} onChange={handleUploadCategoryImage} type='file' id='uploadCategoryImage' className='hidden'/>
+                        </label>
+                        
+                    </div>
+                </div>
 
                 <button
                     className={`
