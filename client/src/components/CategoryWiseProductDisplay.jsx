6f7f32f6454f917a2b2e6764fb78eca95997dcdb
rishiparashar02 <rishivparashar@gmail.com>
2025-06-25 20:59:30 +0530

    changing files

--- client/src/components/CategoryWiseProductDisplay.jsx
+++ client/src/components/CategoryWiseProductDisplay.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { Link, } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import AxiosToastError from '../utils/AxiosToastError'
 import Axios from '../utils/Axios'
 import SummaryApi from '../common/SummaryApi'
@@ -21,13 +21,10 @@ const CategoryWiseProductDisplay = ({ id, name }) => {
             setLoading(true)
             const response = await Axios({
                 ...SummaryApi.getProductByCategory,
-                data: {
-                    id: id
-                }
+                data: { id }
             })
 
             const { data: responseData } = response
-            // console.log(responseData)
 
             if (responseData.success) {
                 setData(responseData.data)
@@ -51,54 +48,48 @@ const CategoryWiseProductDisplay = ({ id, name }) => {
         containerRef.current.scrollLeft -= 200
     }
 
-  const handleRedirectProductListpage = ()=>{
-      const subcategory = subCategoryData.find(sub =>{
-        const filterData = sub.category.some(c => {
-          return c._id == id
+    const handleRedirectProductListpage = () => {
+        const subcategory = subCategoryData.find(sub => {
+            const filterData = sub.category.some(c => c._id === id)
+            return filterData ? true : null
         })
 
-        return filterData ? true : null
-      })
-      const url = `/${valideURLConvert(name)}-${id}/${valideURLConvert(subcategory?.name)}-${subcategory?._id}`
+        const url = `/${valideURLConvert(name)}-${id}/${valideURLConvert(subcategory?.name)}-${subcategory?._id}`
+        return url
+    }
 
-      return url
-  }
+    const redirectURL = handleRedirectProductListpage()
 
-  const redirectURL =  handleRedirectProductListpage()
     return (
-        <div>
+        <div className="pt-24 lg:pt-20"> {/* ✅ Padding added here */}
             <div className='container mx-auto p-4 flex items-center justify-between gap-4'>
                 <h3 className='font-semibold text-lg md:text-xl'>{name}</h3>
-                <Link  to={redirectURL} className='text-green-600 hover:text-green-400'>See All</Link>
+                <Link to={redirectURL} className='text-green-600 hover:text-green-400'>See All</Link>
             </div>
-            <div className='relative flex items-center '>
-                <div className=' flex gap-4 md:gap-6 lg:gap-8 container mx-auto px-4 overflow-x-scroll scrollbar-none scroll-smooth' ref={containerRef}>
+            <div className='relative flex items-center'>
+                <div
+                    className='flex gap-4 md:gap-6 lg:gap-8 container mx-auto px-4 overflow-x-scroll scrollbar-none scroll-smooth'
+                    ref={containerRef}
+                >
                     {loading &&
-                        loadingCardNumber.map((_, index) => {
-                            return (
-                                <CardLoading key={"CategorywiseProductDisplay123" + index} />
-                            )
-                        })
+                        loadingCardNumber.map((_, index) => (
+                            <CardLoading key={"CategorywiseProductDisplay123" + index} />
+                        ))
                     }
 
-
-                    {
-                        data.map((p, index) => {
-                            return (
-                                <CardProduct
-                                    data={p}
-                                    key={p._id + "CategorywiseProductDisplay" + index}
-                                />
-                            )
-                        })
-                    }
-
+                    {data.map((p, index) => (
+                        <CardProduct
+                            data={p}
+                            key={p._id + "CategorywiseProductDisplay" + index}
+                        />
+                    ))}
                 </div>
-                <div className='w-full left-0 right-0 container mx-auto  px-2  absolute hidden lg:flex justify-between'>
+
+                <div className='w-full left-0 right-0 container mx-auto px-2 absolute hidden lg:flex justify-between'>
                     <button onClick={handleScrollLeft} className='z-10 relative bg-white hover:bg-gray-100 shadow-lg text-lg p-2 rounded-full'>
                         <FaAngleLeft />
                     </button>
-                    <button onClick={handleScrollRight} className='z-10 relative  bg-white hover:bg-gray-100 shadow-lg p-2 text-lg rounded-full'>
+                    <button onClick={handleScrollRight} className='z-10 relative bg-white hover:bg-gray-100 shadow-lg p-2 text-lg rounded-full'>
                         <FaAngleRight />
                     </button>
                 </div>
