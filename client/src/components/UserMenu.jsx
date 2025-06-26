9b616b9ca4f5728e65389bc225735df1f385659e
rishiparashar02 <rishivparashar@gmail.com>
2025-06-26 12:35:48 +0530

    changing files

--- client/src/components/UserMenu.jsx
+++ client/src/components/UserMenu.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
 import Divider from './Divider'
+import Axios from '../utils/Axios'
+import SummaryApi from '../common/SummaryApi'
+import { logout } from '../store/userSlice'
+import toast from 'react-hot-toast'
+import AxiosToastError from '../utils/AxiosToastError'
 import { HiOutlineExternalLink } from "react-icons/hi";
+import isAdmin from '../utils/isAdmin'
 
 const UserMenu = ({ close }) => {
   const user = useSelector((state) => state.user)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    try {
+      const response = await Axios({
+        ...SummaryApi.logout
+      })
+      console.log("logout", response)
+      if (response.data.success) {
+        if (close) {
+          close()
+        };
+        dispatch(logout())
+        localStorage.clear()
+        toast.success(response.data.message)
+        navigate("/")
+      };
+    } catch (error) {
+      console.log(error)
+      AxiosToastError(error)
+    }
+  }
 
   const handleClose = () => {
     if (close) {
@@ -25,6 +54,28 @@ const UserMenu = ({ close }) => {
 
       <Divider />
 
+      <div className='text-sm grid gap-1'>
+        {
+          isAdmin(user.role) && (
+            <Link onClick={handleClose} to={"/dashboard/category"} className='px-2 hover:bg-orange-200 py-1'>Category</Link>
+          )
+        }
+
+        {
+          isAdmin(user.role) && (
+            <Link onClick={handleClose} to={"/dashboard/subcategory"} className='px-2 hover:bg-orange-200 py-1'>Sub Category</Link>
+          )
+        }
+
+        {
+          isAdmin(user.role) && (
+            <Link onClick={handleClose} to={"/dashboard/upload-product"} className='px-2 hover:bg-orange-200 py-1'>Upload Product</Link>
+          )
+        }
+
+        <button onClick={handleLogout} className='text-left px-2 hover:bg-orange-200 py-1'>Log Out</button>
+
+      </div>
     </div>
   )
 }
