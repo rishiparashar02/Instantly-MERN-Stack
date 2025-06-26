1f35694d8985eda230760841745cb62101e7fece
rishiparashar02 <rishivparashar@gmail.com>
2025-06-26 12:27:20 +0530

    changing files

--- client/src/components/EditCategory.jsx
+++ client/src/components/EditCategory.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { IoClose } from "react-icons/io5";
 
-const EditCategory = () => {
+const EditCategory = ({close, fetchData,data : CategoryData}) => {
+    const [data,setData] = useState({
+        _id : CategoryData._id,
+        name : CategoryData.name,
+        image : CategoryData.image
+    })
+
+
   return (
-    <div>
-      
+    <section className='fixed top-0 bottom-0 left-0 right-0 p-4 bg-neutral-800 bg-opacity-60 flex items-center justify-center'>
+    <div className='bg-white max-w-4xl w-full p-4 rounded'>
+        <div className='flex items-center justify-between'>
+            <h1 className='font-semibold'>Update Category</h1>
+            <button onClick={close} className='w-fit block ml-auto'>
+                <IoClose size={25}/>
+            </button>
+        </div>
     </div>
+    </section>
   )
 }
 
