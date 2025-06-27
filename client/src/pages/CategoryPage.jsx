7b57d8af26719a6b340f4419688898742427f815
rishiparashar02 <rishivparashar@gmail.com>
2025-06-27 14:39:39 +0530

    added features

---
+++ client/src/pages/CategoryPage.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react'
+import UploadCategoryModel from '../components/UploadCategoryModel'
+import Loading from '../components/Loading'
+import NoData from '../components/NoData'
+import Axios from '../utils/Axios'
+import SummaryApi from '../common/SummaryApi'
+import EditCategory from '../components/EditCategory'
+import CofirmBox from '../components/CofirmBox'
+import toast from 'react-hot-toast'
+import AxiosToastError from '../utils/AxiosToastError'
+
+const CategoryPage = () => {
+    const [openUploadCategory,setOpenUploadCategory] = useState(false)
+    const [loading,setLoading] = useState(false)
+    const [categoryData,setCategoryData] = useState([])
+    const [openEdit,setOpenEdit] = useState(false)
+    const [editData] = useState({
+        name : "",
+        image : "",
+    })
+    const [openConfimBoxDelete,setOpenConfirmBoxDelete] = useState(false)
+    const [deleteCategory,setDeleteCategory] = useState({
+        _id : ""
+    })
+    
+    const fetchCategory = async()=>{
+        try {
+            setLoading(true)
+            const response = await Axios({
+                ...SummaryApi.getCategory
+            })
+            const { data : responseData } = response
+
+            if(responseData.success){
+                setCategoryData(responseData.data)
+            }
+        } catch (error) {
+            
+        }finally{
+            setLoading(false)
+        }
+    }
+
+
+
+  return (
+    <section className=''>
+        <div className='p-2   bg-white shadow-md flex items-center justify-between'>
+            <h2 className='font-semibold'>Category</h2>
+            <button onClick={()=>setOpenUploadCategory(true)} className='text-sm border border-primary-200 hover:bg-primary-200 px-3 py-1 rounded'>Add Category</button>
+        </div>
+        {
+            !categoryData[0] && !loading && (
+                <NoData/>
+            )
+        }
+
+        {
+            loading && (
+                <Loading/>
+            )
+        }
+
+        {
+           openConfimBoxDelete && (
+            <CofirmBox close={()=>setOpenConfirmBoxDelete(false)} cancel={()=>setOpenConfirmBoxDelete(false)} confirm={handleDeleteCategory}/>
+           ) 
+        }
+    </section>
+  )
+}
+
+export default CategoryPage
