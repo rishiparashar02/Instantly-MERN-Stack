8765513b303fc2c2dbff7acbae5a4ea02996afb5
rishiparashar02 <rishivparashar@gmail.com>
2025-06-28 14:00:26 +0530

    changing files

--- client/src/route/index.jsx
+++ client/src/route/index.jsx
@@ -17,6 +17,12 @@ import SubCategoryPage from "../pages/SubCategoryPage";
 import UploadProduct from "../pages/UploadProduct";
 import ProductAdmin from "../pages/ProductAdmin";
 import AdminPermision from "../layouts/AdminPermision";
+import ProductListPage from "../pages/ProductListPage";
+import ProductDisplayPage from "../pages/ProductDisplayPage";
+import CartMobile from "../pages/CartMobile";
+import CheckoutPage from "../pages/CheckoutPage";
+import Success from "../pages/Success";
+import Cancel from "../pages/Cancel";
 
 const router = createBrowserRouter([
     {
@@ -89,7 +95,35 @@ const router = createBrowserRouter([
                     }
                 ]
             },
-            
+            {
+                path : ":category",
+                children : [
+                    {
+                        path : ":subCategory",
+                        element : <ProductListPage/>
+                    }
+                ]
+            },
+            {
+                path : "product/:product",
+                element : <ProductDisplayPage/>
+            },
+            {
+                path : 'cart',
+                element : <CartMobile/>
+            },
+            {
+                path : "checkout",
+                element : <CheckoutPage/>
+            },
+            {
+                path : "success",
+                element : <Success/>
+            },
+            {
+                path : 'cancel',
+                element : <Cancel/>
+            }
         ]
     }
 ])
