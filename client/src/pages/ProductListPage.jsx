4efe2449b1a9f5752450542b4c6a106649c00150
rishiparashar02 <rishivparashar@gmail.com>
2025-06-28 22:23:51 +0530

    final1

--- client/src/pages/ProductListPage.jsx
+++ client/src/pages/ProductListPage.jsx
@@ -73,7 +73,7 @@ const ProductListPage = () => {
   }, [params, AllSubCategory])
 
   return (
-    <section className='sticky top-24'>
+    <section className='sticky top-24 lg:top-20'>
       <div className='container sticky top-24  mx-auto grid grid-cols-[90px,1fr]  md:grid-cols-[200px,1fr] lg:grid-cols-[280px,1fr]'>
         {/**sub category **/}
         <div className=' min-h-[88vh] max-h-[88vh] overflow-y-scroll  grid gap-1 shadow-md scrollbarCustom bg-white py-2'>
